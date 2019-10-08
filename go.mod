44c3313ffcc0ec244a4dd998d865559214d27e8f
Julien Ricard <jarnix@jarnix.com>
2019-10-08 16:53:18 +0100

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module git.webedia-group.net/tools/adsgolib
 
 go 1.13
 
-require github.com/go-chi/chi v4.0.2+incompatible
+require (
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-ini/ini v1.48.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/lintianzhi/graylogd v0.0.0-20180503131252-dc68342f04dc // indirect
+	github.com/robertkowalski/graylog-golang v0.0.0-20151121031040-e5295cfa2827
+	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
+	gopkg.in/ini.v1 v1.48.0 // indirect
+)
---
+++ util/logger.go
@@ -0,0 +1,189 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"runtime"
+	"runtime/debug"
+	"strconv"
+	"strings"
+
+	middleware "git.webedia-group.net/tools/adsgolib/middleware"
+
+	gelf "github.com/robertkowalski/graylog-golang"
+)
+
+// Logger is a struct containing the required resources for logging to screen/logfile/remote syslog
+type Logger struct {
+	realLogger   log.Logger
+	graylog      *gelf.Gelf
+	appName      string
+	appGroupName string
+}
+
+type errorGelf struct {
+	App          string `json:"app"`
+	AppGroup     string `json:"app_group"`
+	FullMessage  string `json:"full_message"`
+	ShortMessage string `json:"short_message"`
+	IPAddress    string `json:"ip_address"`
+	Level        int    `json:"level"`
+	Line         int    `json:"line"`
+	Source       string `json:"source"`
+	URL          string `json:"url"`
+	URLReferer   string `json:"url_referer"`
+	UserAgent    string `json:"user_agent"`
+}
+
+/*
+0 critical : l'app ne peut pas ou plus fonctionner
+3 error : http 500, impossible de répondre au client
+5 warning : on a catché l'erreur
+6 notice : hash déjà utilisé, tout ce qui n'empêche pas de continuer ou qui n'est pas une erreur en soi
+7 debug : pour nous, pour comprendre ce qui se passe dans un algo en fonction des paramètres par exemple
+*/
+var logLevels = map[string]int{
+	"critical": 0,
+	"error":    3,
+	"warning":  5,
+	"notice":   6,
+	"debug":    7,
+}
+
+// Init will instantiate our logger, setup the graylog connection
+func (logger *Logger) Init(graylogIPStr string, graylogPortStr string, appName string, appGroupName string) error {
+	graylogPort, err := strconv.Atoi(graylogPortStr)
+	if err != nil {
+		return err
+	}
+	logger.graylog = gelf.New(gelf.Config{
+		GraylogHostname: graylogIPStr,
+		GraylogPort:     graylogPort,
+	})
+	logger.appName = appName
+	logger.appGroupName = appGroupName
+	return nil
+}
+
+// Critical is used for errors that cannot be recovered
+func (logger *Logger) Critical(msg string, w http.ResponseWriter, r *http.Request) error {
+	logger.sendToGraylog(msg, r)
+	if w != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Software Failure. Press left mouse button to continue.\nGuru Meditation #00000025.65045338"))
+	}
+	return nil
+}
+
+// Error is used for errors that cannot be recovered but we can still live with them
+func (logger *Logger) Error(msg string, w http.ResponseWriter, r *http.Request) error {
+	logger.sendToGraylog(msg, r)
+	if w != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Software Failure. Press left mouse button to continue.\nGuru Meditation #00000025.65045338"))
+	}
+	return nil
+}
+
+// NotFound is used when a content or corresponding value was not found
+func (logger *Logger) NotFound(msg string, w http.ResponseWriter, r *http.Request) error {
+	logger.sendToGraylog(msg, r)
+	if w != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 - not found"))
+	}
+	return nil
+}
+
+// Warning is used for errors that have been recovered
+func (logger *Logger) Warning(msg string, w http.ResponseWriter, r *http.Request) error {
+	logger.sendToGraylog(msg, r)
+	return nil
+}
+
+// Notice is mainly used internally for debugging to console
+func (logger *Logger) Notice(msg string, w http.ResponseWriter, r *http.Request) error {
+	logger.sendToGraylog(msg, r)
+	return nil
+}
+
+// Debug is mainly used internally for debugging to console
+func (logger *Logger) Debug(msg string, w http.ResponseWriter, r *http.Request) error {
+	logger.sendToGraylog(msg, r)
+	return nil
+}
+
+// sendToGraylog formats and sends a message to graylog along with the filename, line number, etc
+func (logger *Logger) sendToGraylog(msg string, r *http.Request) {
+	pc, _, _, ok1 := runtime.Caller(1)
+	details := runtime.FuncForPC(pc)
+	if ok1 && details != nil {
+
+		fmt.Println("error sent to Graylog")
+
+		// details.Name() contient le nom de la méthode appelée juste avant
+		regex, _ := regexp.Compile(".[a-z]+$")
+		previousFunction := regex.FindString(details.Name())
+		if previousFunction != "" {
+			previousFunction = strings.ToLower(previousFunction)
+			logLevel, levelFound := logLevels[previousFunction]
+			if !levelFound {
+				logLevel = 7
+			}
+
+			_, fileName, lineNumber, ok2 := runtime.Caller(2)
+
+			var ok3 = true
+			if ok2 {
+				if strings.HasSuffix(fileName, "recoverer.go") {
+					_, fileName, lineNumber, ok3 = runtime.Caller(5)
+				}
+			}
+			if ok3 {
+
+				// default values
+				logIP := "unknown"
+				logReferer := "unknown"
+				logUserAgent := "unknown"
+				logURL := "unknown"
+
+				if r != nil {
+					ctx := r.Context()
+					logIP, _ = ctx.Value(middleware.ContextKeyRequestIP).(string)
+					logReferer, _ = ctx.Value(middleware.ContextKeyReferer).(string)
+					logUserAgent, _ = ctx.Value(middleware.ContextKeyUserAgent).(string)
+					logURL = r.URL.RequestURI()
+				}
+
+				debugStack := debug.Stack()
+
+				errorToLog := errorGelf{
+					App:          logger.appName,
+					AppGroup:     logger.appGroupName,
+					ShortMessage: msg,
+					FullMessage:  string(debugStack),
+					IPAddress:    logIP,
+					Level:        logLevel,
+					Line:         lineNumber,
+					Source:       fileName,
+					URL:          logURL,
+					URLReferer:   logReferer,
+					UserAgent:    logUserAgent,
+				}
+
+				fmt.Println("sent to graylog maybe")
+
+				errorToLogJSON, errJSON := json.Marshal(errorToLog)
+				if errJSON == nil {
+					logger.graylog.Log(string(errorToLogJSON))
+				}
+
+			}
+		}
+
+	}
+
+}
